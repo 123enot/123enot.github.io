9b7b02aad2ec3f7721f1158173fa1b2ec12463ed
Volodymyr Manko <vmanko1@gmail.com>
2016-10-14 11:43:47 +0300

    t02. boolean algebra. person's info in number. done, with objects.

---
+++ markup/spd-university/leo-bitwise-operators/t02. bitwise-operators.js
@@ -0,0 +1,91 @@
+//   ----------------------------------
+//                        male female
+//   1. sex               1    0
+//   ----------------------------------
+//                        yes  no
+//   2. marriage          1    0
+//   ----------------------------------
+//                        <30  [25; 30]
+//   3. age:              1    0
+//   ----------------------------------
+//                        no   >=3
+//   4. children:         1    0
+//   ----------------------------------
+//                        yes  no
+//   5. higher education: 1    0
+
+// A: 1_101
+// B: _0_10
+// C: 11000
+
+let array = [],
+    maxBit = 5 + 1,  // The number of bits + controlling bit
+    maxNum = parseInt(new Array(maxBit + 1).join('1'), 2);
+
+for (let i = 0; i < 500; i++) {
+    array.push( Math.round(Math.random()*maxNum) );
+}
+
+console.log('initial array: ' + array);
+
+let a = {
+        '1': '1',
+        '2': '0',
+        '3': '1',
+        '5': '1'
+    },
+
+    b = {
+        '1': '0',
+        '2': '1',
+        '4': '0'
+    },
+
+    c = {
+        '1': '0',
+        '2': '0',
+        '3': '0',
+        '4': '1',
+        '5': '1'
+    };
+
+a[maxBit] = '1';
+b[maxBit] = '1';
+c[maxBit] = '1';
+
+let arrA = array.filter(function(item) {
+        if (item.toString(2).length >= maxBit) {
+            let num = item.toString(2).slice(-maxBit).split('');
+
+            for (let key in a) {
+                if (a[key] !== num[num.length - key]) return false;
+            }
+            return true;
+        }
+    }),
+
+    arrB = array.filter(function(item) {
+        if (item.toString(2).length >= maxBit) {
+            let num = item.toString(2).slice(-maxBit).split('');
+
+            for (let key in b) {
+                if (b[key] !== num[num.length - key]) return false;
+            }
+            return true;
+        }
+    }),
+
+    arrC = array.filter(function(item) {
+        if (item.toString(2).length >= maxBit) {
+            let num = item.toString(2).slice(-maxBit).split('');
+
+            for (let key in c) {
+                if (c[key] !== num[num.length - key]) return false;
+            }
+            return true;
+        }
+    });
+
+console.log('array A: ' + arrA);
+console.log('array B: ' + arrB);
+console.log('array C: ' + arrC);
