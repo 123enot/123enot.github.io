72d540dab798df136fe37db258d0759b5730a69c
Volodymyr Manko <vmanko1@gmail.com>
2016-12-09 17:30:16 +0200

    boolean algebra.

--- markup/spd-university/leo-bitwise-operators/t02. bitwise-operators.js
+++ markup/spd-university/leo-bitwise-operators/t02. bitwise-operators.js
@@ -1,26 +1,30 @@
-//   ----------------------------------
+//   ---------------------------------
 //                        male female
 //   1. sex               1    0
-//   ----------------------------------
+//   ---------------------------------
 //                        yes  no
 //   2. marriage          1    0
-//   ----------------------------------
-//                        <30  [25; 30]
-//   3. age:              1    0
-//   ----------------------------------
-//                        no   >=3
-//   4. children:         1    0
-//   ----------------------------------
+//   ---------------------------------
 //                        yes  no
-//   5. higher education: 1    0
+//   3. higher education  1    0
+//   ---------------------------------
+//                        0 - 7   
+//   children             bits 4-6
+//   ---------------------------------
+//                        0 - 63
+//   age                  bits 7-12
+//   ------------------------------------
+//                pure values:
+//                ------------
+// isMale       : 000000000001  --> number 1
+// isMarried    : 000000000010  --> number 2
+// hasEducation : 000000000100  --> number 4
 
-// A: 1_101
-// B: _0_10
-// C: 11000
-
-let array = [],
-    maxBit = 5 + 1,  // The number of bits + controlling bit
-    maxNum = parseInt(new Array(maxBit + 1).join('1'), 2);
+let isMale = item => !!(item & 1),
+    isMarried = item => !!(item & 2),
+    hasEducation = item => !!(item & 4),
+    array = [],
+    maxNum = 4095; // 111111111111 --> number 4095
 
 for (let i = 0; i < 500; i++) {
     array.push( Math.round(Math.random()*maxNum) );
@@ -28,50 +32,39 @@ for (let i = 0; i < 500; i++) {
 
 console.log('initial array: ' + array);
 
-let a = {
-        '1': '1',
-        '2': '0',
-        '3': '1',
-        '5': '1'
-    },
-
-    b = {
-        '1': '0',
-        '2': '1',
-        '4': '0'
-    },
-
-    c = {
-        '1': '0',
-        '2': '0',
-        '3': '0',
-        '4': '1',
-        '5': '1'
-    };
-
-a[maxBit] = '1';
-b[maxBit] = '1';
-c[maxBit] = '1';
-
-let arrA = filterInfo(array, a),
-
-    arrB = filterInfo(array, b),
-
-    arrC = filterInfo(array, c);
-
-function filterInfo(arr, obj) {
-    return arr.filter(function(item) {
-        if (item.toString(2).length >= maxBit) {
-            let num = item.toString(2).slice(-maxBit).split('');
-
-            for (let key in obj) {
-                if (obj[key] !== num[num.length - key]) return false;
-            }
-            return true;
-        }
+let arrA = array.filter(function(item) {
+        return isMale(item) && !isMarried(item) && hasEducation(item) && (item >> 6 < 30);
+    }),
+    arrB = array.filter(function(item) {
+        return !isMale(item) && isMarried(item) && ( item >> 3 & 2 && item >> 3 & 1 || item >> 3 & 4 );
+    }),
+    arrC = array.filter(function(item) {
+        return !isMale(item) && !isMarried(item) && hasEducation(item) && !(item >> 3 & 7) && (item >> 6 >= 25 && item >> 6 <= 30);
     });
-}
 
 console.log('array A: ' + arrA);
 console.log('array B: ' + arrB);
 console.log('array C: ' + arrC);
+
+// Visual check for mistakes:
+let a = {},
+    b = {},
+    c = {};
+
+function listValues(obj, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!obj[arr[i]]) obj[arr[i]] = arr[i].toString(2);
+    }
+    for (let key in obj) {
+        console.log(key + ': ' + obj[key]);
+    }
+}
+
+console.log('in array A: ');
+listValues(a, arrA);
+
+console.log('in array B: ');
+listValues(b, arrB);
+
+console.log('in array C: ');
+listValues(c, arrC);
